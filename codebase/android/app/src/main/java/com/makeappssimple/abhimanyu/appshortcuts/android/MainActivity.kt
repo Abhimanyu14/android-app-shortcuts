aeb76d265b5d6c6dc3f2b40b65a933a643684521
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-07-21 11:00:07 +0530

    created android project

---
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/appshortcuts/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.makeappssimple.abhimanyu.appshortcuts.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
